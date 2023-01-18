24bff4c577f7d85979479d7cfb31a5cdc775c587
lsun01 <lsun0547@gmail.com>
2023-01-17 21:45:35 -0500

    String-1

--- string1.py
+++ string1.py
@@ -1,3 +1,48 @@
-#String-2
-############################
+# String-1
+##############################
 
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+def hello_name(name):
+    return 'Hello ' + name +'!'
+
+# Given two strings, a and b, return the result of putting them together in the order abba,
+# e.g. "Hi" and "Bye" returns "HiByeByeHi".
+def make_abba(a, b):
+    return a + b + b + a
+
+# The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this
+# example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and
+# word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+def make_tags(tag, word):
+    return '<' + tag + '>' + word + '</' + tag + '>'
+
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where
+# the word is in the middle of the out string, e.g. "<<word>>".
+def make_out_word(out, word):
+    return out[:2] + word + out[2:4]
+
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original
+# string. The string length will be at least 2.
+def extra_end(str):
+    return str[-2:] * 3
+
+# Given a string, return a version without the first and last char, so "Hello" yields "ell". The
+# string length will be at least 2.
+def without_end(str):
+    return str[1:-1]
+
+# Given 2 strings, a and b, return a string of the form short+long+short, with the shorter
+# string on the outside and the longer string on the inside. The strings will not be the same
+# length, but they may be empty (length 0).
+def combo_string(a, b):
+    return a + b + a if len(a) < len(b) else  b + a + b
+
+# Given 2 strings, return their concatenation, except omit the first char of each. The strings
+# will be at least length 1.
+def non_start(a, b):
+    return a[1:] + b[1:]
+
+# Given a string, return a "rotated left 2" version where the first 2 chars are moved to the
+# end. The string length will be at least 2.
+def left2(str):
+    return str[2:] + str[:2]
--- warmup2.py
+++ warmup2.py
@@ -1,74 +1,47 @@
-#Warmup-2
-############################
+# Warmup-2
+##############################
 
-#Given a string and a non-negative int n, return a larger string that is n copies of the
-#original string.
+# Given a string and a non-negative int n, return a larger string that is n copies of the
+# original string.
 def string_times(str, n):
     return str * n
 
-#Given a string and a non-negative int n, we'll say that the front of the string is the first 3
-#chars, or whatever is there if the string is less than length 3. Return n copies of the front.
+# Given a string and a non-negative int n, we'll say that the front of the string is the first 3
+# chars, or whatever is there if the string is less than length 3. Return n copies of the front.
 def front_times(str, n):
-    return str[: 3] * n
+    return str[:3] * n
 
-#Given a string, return a new string made of every other char starting with the first, so
-#"Hello" yields "Hlo".
+# Given a string, return a new string made of every other char starting with the first, so
+# "Hello" yields "Hlo".
 def string_bits(str):
-    result = ""
-    for i in range(len(str)):
-        if i % 2 == 0:
-            result += str[i]
-    return result
+    return str[::2]
 
-#Given a non-empty string like "Code" return a string like "CCoCodCode".
+# Given a non-empty string like "Code" return a string like "CCoCodCode".
 def string_splosion(str):
-    result = ''
-    for i in range(len(str)):
-        result += str[: i + 1]
-    return result
+    return ''.join([str[:i + 1] for i in range(len(str))])
 
-#Given a string, return the count of the number of times that a substring length 2 appears
-#in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't
-#count the end substring).
+# Given a string, return the count of the number of times that a substring length 2 appears
+# in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't
+# count the end substring).
 def last2(str):
-    count = 0
-    last = str[-2:]
-    for i in range(len(str)-2):
-        sub = str[i : i + 2]
-        if sub == last:
-            count = count + 1
-    return count
+    return sum(1 for i in range(len(str) - 2) if str[-2:] == str[i:i + 2])
 
-#Given an array of ints, return the number of 9's in the array.
+# Given an array of ints, return the number of 9's in the array.
 def array_count9(nums):
-    count = 0
-    for i in range(len(nums)):
-        if nums[i] == 9:
-            count += 1
-    return count
+    return sum(1 for num in nums if num == 9)
 
-#Given an array of ints, return True if one of the first 4 elements in the array is a 9. The
-#array length may be less than 4.
+# Given an array of ints, return True if one of the first 4 elements in the array is a 9. The
+# array length may be less than 4.
 def array_front9(nums):
-    for i in range(len(nums) if len(nums) < 4 else 4):
-        if nums[i] == 9:
-            return True
-    return False
+    return 9 in nums[:4]
 
-#Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array
-#somewhere.
+# Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array
+# somewhere.
 def array123(nums):
-    for i in range(len(nums)-2):
-        if nums[i] == 1 and nums[i + 1] == 2 and nums[i + 2] == 3:
-            return True
-    return False
+    return (1, 2, 3) in zip(nums, nums[1:], nums[2:])
 
-#Given 2 strings, a and b, return the number of the positions where they contain the same
-#length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
-#substrings appear in the same place in both strings.
+# Given 2 strings, a and b, return the number of the positions where they contain the same
+# length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+# substrings appear in the same place in both strings.
 def string_match(a, b):
-    count = 0
-    for i in range(min(len(a), len(b)) - 1):
-        if a[i : i + 2] == b[i : i + 2]:
-            count += 1
-    return count
+    return sum(1 for i in range(min(len(a), len(b)) - 1) if a[i:i + 2] == b[i:i + 2])
