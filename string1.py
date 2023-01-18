f85bdb98ca4c3feab6adf07f80ca8b4cc144937a
lsun01 <lsun0547@gmail.com>
2023-01-17 21:49:26 -0500

    edit String-1

--- string1.py
+++ string1.py
@@ -19,12 +19,22 @@ def make_tags(tag, word):
 # Given an "out" string length 4, such as "<<>>", and a word, return a new string where
 # the word is in the middle of the out string, e.g. "<<word>>".
 def make_out_word(out, word):
-    return out[:2] + word + out[2:4]
+    return out[:2] + word + out[2:]
 
 # Given a string, return a new string made of 3 copies of the last 2 chars of the original
 # string. The string length will be at least 2.
 def extra_end(str):
     return str[-2:] * 3
+
+# Given a string, return the string made of its first two chars, so the String "Hello" yields
+# "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and
+# the empty string "" yields the empty string "".
+def first_two(str):
+    return str[:2]
+
+#Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+def first_half(str):
+    return str[:len(str)/2]
 
 # Given a string, return a version without the first and last char, so "Hello" yields "ell". The
 # string length will be at least 2.
